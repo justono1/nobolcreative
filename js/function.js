75f4a858d75ae8b34dc559996e7e23c9a85d12b1
Justin Gauthier <justin@agoodconcept.com>
2014-10-25 12:44:08 -0400

    Final Commit Before Animations

--- js/function.js
+++ js/function.js
@@ -3,7 +3,13 @@ $(document).ready(function() {
 	$(".services-select").fancySelect();
 	$(".budget-select").fancySelect();
 
-	$.validate({
+  $(".slideshow").cycle({
+  	fx: "fade",
+  	timeout: 6000,
+  	pager: '#pager'
+  });
+
+  $.validate({
     validateOnBlur : false,
     showHelpOnFocus : false,
     modules : 'file'
