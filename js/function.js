cca526b9f443c86703f9901a1f22751e36df774d
Justin Gauthier <justin@agoodconcept.com>
2014-10-26 15:03:29 -0400

    Going mobile, but habing issue safe commit

--- js/function.js
+++ js/function.js
@@ -25,6 +25,37 @@ $(document).ready(function() {
   $(window).resize(function() {
   	$(".top-container").css("top", $(window).height());
   });
+
+  $(".icon-contact").on("touchstart click", function() {
+    $("html, body").animate({scrollTop: $(".contact").offset().top - 78}, 800);
+    return false;
+  });
+
+  function closeNotification() {
+    $(".notification").delay(2000).slideUp("slow");
+  }
+
+  $("#ajaxForm").submit(function() {
+    var postData = $(this).serializeArray();
+    var formUrl = $(this).attr("action");
+
+    $.ajax({
+      url: formUrl,
+      type: "POST",
+      data: postData,
+      success: function(data) {
+        $(".notification").css("background-color", "#00bf61").slideDown("slow");
+        $(".notification p").html(data);
+        closeNotification();
+      },
+      error: function(data) {
+        $(".notification").css("background-color", "#e12e34").slideDown("slow");
+        $(".notification p").html(data);
+        closeNotification();
+      }
+    });
+    return false;
+  });
 	
 	var s = skrollr.init();
 
--- process/index.php
+++ process/index.php
@@ -1,4 +1,29 @@
 <?php
 
+	$to = "justin@wearenobol.com";
+	$phone = $_POST['phone'];
+	$sender = $_POST['name'];
+	$from = $_POST['email'];
+	$company = $_POST['company'];
+	$message = $_POST['message'];
+	$budget = $_POST['budget'];
+	$services = $_POST['services'];
+
+	
+	$headers = "From: ".$from."\r\nReply-To: ".$from."\r\n";
+	$body = "Message from: ".$sender."\r\nPhone: ".$phone."\r\nEmail: ".$from."\r\n";
+	$body.= "Sent: ".date('l F jS')."\r\n\r\n";
+	
+	$body.= $message;
+
+	$subject = "Website Contact Form";
+	
+	$sent = mail($to,$subject,$body,$headers);
+	
+	if($sent) {
+		echo json_encode("Your message has been sent successfully.");
+	} else {
+		echo json_encode("Whoops there was an error, please try again.");
+	}
 
 ?>
